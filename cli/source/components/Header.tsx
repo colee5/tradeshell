a9c7870ae616b7c67825172ab185a29a9409f633
colee5 <oliver.gorgiev11@gmail.com>
2026-01-06 16:44:53 +0100

    style: logo in header

--- cli/source/components/Header.tsx
+++ cli/source/components/Header.tsx
@@ -1,12 +1,17 @@
 import { Text } from 'ink';
+import figlet from 'figlet';
 import React from 'react';
 
-// TODO: Redesign
+const LOGO = figlet.textSync('> TradeShell', {
+	font: 'Standard',
+	horizontalLayout: 'default',
+});
+
 export function Header() {
 	return (
 		<>
-			<Text color="cyan" bold>
-				=== TradeShell Interactive Mode ===
+			<Text color="#FCCB3D" bold>
+				{LOGO}
 			</Text>
 			<Text color="gray">Type commands below. Type '/' for suggestions. Type 'exit' to quit.</Text>
 			<Text> </Text>
