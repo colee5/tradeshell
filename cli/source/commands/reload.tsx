77820e3dd8798d9991a0b31695a50496a6403952
colee5 <oliver.gorgiev11@gmail.com>
2026-01-07 18:08:40 +0100

    feat: index.tsx refactor to react component commands + subcommands command example

--- cli/source/commands/reload.tsx
+++ cli/source/commands/reload.tsx
@@ -1,5 +1,5 @@
-import { useApp } from 'ink';
-import { useEffect } from 'react';
+import { Text, useApp } from 'ink';
+import React, { useEffect } from 'react';
 import { RELOAD_ERROR_CODE } from '../lib/constants.js';
 
 export function Reload() {
@@ -9,4 +9,6 @@ export function Reload() {
 		exit();
 		process.exit(RELOAD_ERROR_CODE);
 	}, [exit]);
+
+	return <Text>Restarting CLI...</Text>;
 }
