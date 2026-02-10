9bd99aea8c7a69dad08eadfca96e1154e134e99f
colee5 <oliver.gorgiev11@gmail.com>
2026-02-10 08:44:25 +0100

    fix: export only needed stuff from packages/core

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,51 +1,9 @@
 // Services
+export { BlockchainService } from './services/blockchain.service.js';
 export { ConfigService } from './services/config.service.js';
 export { WalletService } from './services/wallet.service.js';
-export { BlockchainService } from './services/blockchain.service.js';
 
-// Errors
-export { CoreError, NotFoundError, UnauthorizedError, BadRequestError } from './services/errors.js';
-
-// Logger
-export { createLogger, type Logger } from './services/logger.js';
-
-// Types (Zod schemas + inferred types)
-export {
-	configSchema,
-	llmConfigSchema,
-	blockchainConfigSchema,
-	type Config,
-	type LlmConfig,
-	type BlockchainConfig,
-} from './types/config.types.js';
-export {
-	walletInfoSchema,
-	walletStatusSchema,
-	addWalletInputSchema,
-	type WalletInfo,
-	type WalletStatus,
-	type AddWalletInput,
-} from './types/wallet.types.js';
-
-// Constants
-export { ChainId, CHAIN_BY_ID } from './constants/chains.js';
+// Types
 export type { Chain } from './constants/chains.js';
-export {
-	TRADESHELL_DIR,
-	CONFIG_PATH,
-	WALLETS_FILE,
-	LOGS_FILE,
-	hashRegex,
-} from './constants/paths.js';
-export { CONFIG_EVENTS, WALLET_EVENTS } from './constants/events.js';
-
-// Utils
-export {
-	generateMasterKey,
-	encryptMasterKey,
-	decryptMasterKey,
-	encryptPrivateKey,
-	decryptPrivateKey,
-	type EncryptedData,
-	type MasterKeyData,
-} from './utils/crypto.utils.js';
+export { type BlockchainConfig, type Config, type LlmConfig } from './types/config.types.js';
+export { type WalletInfo, type WalletStatus } from './types/wallet.types.js';
