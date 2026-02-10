e033458d6bacafc996df84b69c95168d73487e00
colee5 <oliver.gorgiev11@gmail.com>
2026-02-10 09:33:07 +0100

    docs: comment on paths.ts

--- packages/core/src/constants/paths.ts
+++ packages/core/src/constants/paths.ts
@@ -1,8 +1,17 @@
 import * as os from 'os';
 import * as path from 'path';
 
+// ~/.tradeshell
 export const TRADESHELL_DIR = path.join(os.homedir(), '.tradeshell');
+
+// ~/.tradeshell/config.json
 export const CONFIG_PATH = path.join(TRADESHELL_DIR, 'config.json');
+
+// ~/.tradeshell/wallets.json
 export const WALLETS_FILE = path.join(TRADESHELL_DIR, 'wallets.json');
+
+// ~/.tradeshell/debug.log
 export const LOGS_FILE = path.join(TRADESHELL_DIR, 'debug.log');
+
+// CONSTANTS
 export const hashRegex: RegExp = /^0x[0-9a-fA-F]{64}$/;
