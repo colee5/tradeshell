6fa68266e18cacbcfec00c661c0e7ff31d10373e
colee5 <oliver.gorgiev11@gmail.com>
2026-02-11 12:28:18 +0100

    fix: explicitly add wallet.switched event

--- packages/core/src/services/blockchain.service.ts
+++ packages/core/src/services/blockchain.service.ts
@@ -23,6 +23,7 @@ export class BlockchainService {
 		// Wire up event listeners
 		this.emitter.on(CONFIG_EVENTS.BLOCKCHAIN_UPDATED, () => this.handleBlockchainConfigUpdated());
 		this.emitter.on(WALLET_EVENTS.UNLOCKED, () => this.handleWalletUnlocked());
+		this.emitter.on(WALLET_EVENTS.SWITCHED, () => this.handleWalletSwitched());
 		this.emitter.on(WALLET_EVENTS.LOCKED, () => this.handleWalletLocked());
 	}
 
@@ -90,6 +91,7 @@ export class BlockchainService {
 		}
 
 		const account = this.walletService.getActiveAccount();
+
 		if (!account) {
 			throw new Error('No wallet unlocked');
 		}
@@ -125,6 +127,18 @@ export class BlockchainService {
 
 	private handleWalletUnlocked() {
 		this.logger.log('Wallet unlocked, initializing wallet client...');
+
+		if (this.isInitialized()) {
+			try {
+				this.initializeWalletClient();
+			} catch (error) {
+				this.logger.error('Failed to initialize wallet client', error);
+			}
+		}
+	}
+
+	private handleWalletSwitched() {
+		this.logger.log('Active wallet switched, reinitializing wallet client...');
 
 		if (this.isInitialized()) {
 			try {
