44ca9c3f375ce602e5f01d66e96a6b07f1d9ed83
colee5 <oliver.gorgiev11@gmail.com>
2026-02-20 17:29:54 +0100

    refactor: errors with enums

--- packages/core/src/services/errors.ts
+++ packages/core/src/services/errors.ts
@@ -1,7 +1,15 @@
+export enum ErrorCode {
+	NotFound = 'NOT_FOUND',
+	NotInitialized = 'NOT_INITIALIZED',
+	Unauthorized = 'UNAUTHORIZED',
+	BadRequest = 'BAD_REQUEST',
+	LlmError = 'LLM_ERROR',
+}
+
 export class CoreError extends Error {
 	constructor(
 		message: string,
-		public readonly code: string,
+		public readonly code: ErrorCode,
 	) {
 		super(message);
 		this.name = 'CoreError';
@@ -10,21 +18,38 @@ export class CoreError extends Error {
 
 export class NotFoundError extends CoreError {
 	constructor(message: string) {
-		super(message, 'NOT_FOUND');
+		super(message, ErrorCode.NotFound);
 		this.name = 'NotFoundError';
+	}
+}
+
+export class NotInitializedError extends CoreError {
+	constructor(message: string) {
+		super(message, ErrorCode.NotInitialized);
+		this.name = 'NotInitializedError';
 	}
 }
 
 export class UnauthorizedError extends CoreError {
 	constructor(message: string) {
-		super(message, 'UNAUTHORIZED');
+		super(message, ErrorCode.Unauthorized);
 		this.name = 'UnauthorizedError';
 	}
 }
 
 export class BadRequestError extends CoreError {
 	constructor(message: string) {
-		super(message, 'BAD_REQUEST');
+		super(message, ErrorCode.BadRequest);
 		this.name = 'BadRequestError';
+	}
+}
+
+export class LlmError extends CoreError {
+	constructor(
+		message: string,
+		public readonly statusCode?: number,
+	) {
+		super(message, ErrorCode.LlmError);
+		this.name = 'LlmError';
 	}
 }
